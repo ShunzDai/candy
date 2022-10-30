80cfacc05dd485c668503e19dbaa8100d5fb5418
ShunzDai <shunzdai@outlook.com>
2022-10-30 13:49:58 +0800

    style(*):修改目录结构

--- demo/esp32/main/main.cpp
+++ demo/esp32/main/main.cpp
@@ -1,16 +1,5 @@
-#include "mid_os.h"
-#include "src/core/candy_parser.h"
 #include <stdio.h>
-
-os::timer_t t([](os::timer_t *timer) {
-  auto tick = os::tick_us();
-  auto parser = candy_parser_create("(((1 + (2 * 3)/(4 + 5))*(6 - 7) + (8 + 9) * 10)/11) - 12");
-  printf("tick %llu us\n", os::tick_us() - tick);
-  candy_parser_delete(&parser);
-  timer->start(1000);
-}, nullptr, "timer");
 
 extern "C" void app_main(void) {
   printf("into %s\n", __func__);
-  t.start(1000);
 }
