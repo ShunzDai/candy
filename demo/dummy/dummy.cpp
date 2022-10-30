8eb868593f67ffe166c4226b77824b348994c8fb
ShunzDai <shunzdai@outlook.com>
2022-10-30 09:07:48 +0800

    feat(cpp):新增c注册器

--- demo/dummy/dummy.cpp
+++ demo/dummy/dummy.cpp
@@ -12,19 +12,19 @@ void func2(int arg1, float arg2, std::string arg3) {
   printf("into %s, arg1 %d, arg2 %.3f, arg3 '%s'\n", __FUNCTION__, arg1, arg2, arg3.data());
 }
 
-std::string func3() {
+int func3() {
   printf("into %s\n", __FUNCTION__);
-  return "test3 return";
+  return 314;
 }
 
 std::tuple<int, float, std::string> func4() {
   printf("into %s\n", __FUNCTION__);
-  return {314, 3.14f, "test3 return"};
+  return {314, 3.14f, __FUNCTION__};
 }
 
 std::tuple<int, float, std::string> func5(int arg1, float arg2, std::string arg3) {
   printf("into %s, arg1 %d, arg2 %.3f, arg3 '%s'\n", __FUNCTION__, arg1, arg2, arg3.data());
-  return {314, 3.14f, "test3 return"};
+  return {314, 3.14f, __FUNCTION__};
 }
 
 int main(int argc, char *argv[]) {
@@ -33,13 +33,15 @@ int main(int argc, char *argv[]) {
   candy cdy;
   /* auto res1 = */ cdy.call("func1");
   /* auto res2 = */ cdy.call("func2", 314, 3.14f, "str from cpp");
-  /* auto res3 = */ cdy.call<std::string>("func3", 314, 3.14f, "str from cpp");
+  /* auto res3 = */ cdy.call<int>("func3");
   /* auto res4 = */ cdy.call<int, float, std::string>("func4");
   /* auto res5 = */ cdy.call<int, float, std::string>("func5", 314, 3.14f, "str from cpp");
-  cdy.call(func1);
-  cdy.call(func2);
-  cdy.call(func3);
-  cdy.call(func4);
-  cdy.call(func5);
+  cdy.regist(
+    std::pair{"func1", func1},
+    std::pair{"func2", func2},
+    std::pair{"func3", func3},
+    std::pair{"func4", func4},
+    std::pair{"func5", func5}
+  );
   return 0;
 }
