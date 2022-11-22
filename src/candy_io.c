85541204cb0a6cc30a4e5e0e26775b6c2568151b
ShunzDai <shunzdai@outlook.com>
2022-11-22 10:07:08 +0800

    perf(io):现在缓冲区大小不再呈指数增长, 而是呈线性增长 doc(config):增加一些注释

--- src/candy_io.c
+++ src/candy_io.c
@@ -36,14 +36,13 @@ char candy_io_read(candy_io_t *self) {
 
 void candy_io_write(candy_io_t *self, char ch) {
   self->buffer[self->w++] = ch;
-  if (self->size - self->w < self->size / 4) {
-    int size = self->size * 2;
-    char *buffer = (char *)calloc(size, sizeof(char));
+  if (self->size - self->w < CANDY_IO_DEFAULT_BUFFER_SIZE / 2) {
+    char *buffer = (char *)calloc(self->size + CANDY_IO_DEFAULT_BUFFER_SIZE, sizeof(char));
     memcpy(buffer, self->buffer, self->size);
-    self->reader(buffer + self->size, self->size, self->ud);
+    self->reader(buffer + self->size, CANDY_IO_DEFAULT_BUFFER_SIZE, self->ud);
     free(self->buffer);
     self->buffer = buffer;
-    self->size = size;
+    self->size += CANDY_IO_DEFAULT_BUFFER_SIZE;
   }
 }
 
