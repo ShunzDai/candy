ccd1964fa1575354853e7a0718babbe3e35bfe4a
ShunzDai <shunzdai@outlook.com>
2023-02-10 20:30:34 +0800

    refactor(wrap):重构

--- test/test_wrap.cpp
+++ test/test_wrap.cpp
@@ -14,55 +14,20 @@
   * limitations under the License.
   */
 #include "gtest/gtest.h"
-#include "src/candy_lib.h"
 #include "src/candy_wrap.h"
 
-TEST(wrap, lifecycle) {
-  struct candy_wrap *wrap = candy_wrap_none(0);
-  candy_wrap_delete(&wrap);
-  EXPECT_EQ((uint64_t)wrap, (uint64_t)NULL);
+TEST(wrap, sstring) {
+  candy_wrap_t wrap;
+  candy_wrap_init_string(&wrap, (char *)"hello", sizeof("hello"));
+  EXPECT_EQ(wrap.size, sizeof("hello"));
+  EXPECT_STREQ(candy_wrap_get_string(&wrap, NULL), (char *)"hello");
+  candy_wrap_deinit(&wrap);
 }
 
-TEST(wrap, string) {
-  struct candy_wrap *wrap = candy_wrap_string(0, "hello world", sizeof("hello world"));
-  EXPECT_STREQ((char *)candy_wrap_get_string(wrap)->data, (char *)"hello world");
-  EXPECT_EQ(candy_wrap_view(wrap)->size, sizeof("hello world"));
-  candy_wrap_set_string(&wrap, "bye world", sizeof("bye world"));
-  EXPECT_STREQ((char *)candy_wrap_get_string(wrap)->data, (char *)"bye world");
-  EXPECT_EQ(candy_wrap_view(wrap)->size, sizeof("bye world"));
-  candy_wrap_delete(&wrap);
-  EXPECT_EQ((uint64_t)wrap, (uint64_t)NULL);
-}
-
-TEST(wrap, integer) {
-  struct candy_wrap *wrap = candy_wrap_integer(0, 114514);
-  EXPECT_EQ(candy_wrap_get_integer(wrap), 114514);
-  candy_wrap_set_integer(&wrap, 1919810);
-  EXPECT_EQ(candy_wrap_get_integer(wrap), 1919810);
-  EXPECT_FLOAT_EQ(candy_wrap_get_float(wrap), 1919810.0f);
-  candy_wrap_delete(&wrap);
-  EXPECT_EQ((uint64_t)wrap, (uint64_t)NULL);
-}
-
-TEST(wrap, float) {
-  struct candy_wrap *wrap = candy_wrap_float(0, 6.626f);
-  EXPECT_FLOAT_EQ(candy_wrap_get_float(wrap), 6.626f);
-  EXPECT_EQ(candy_wrap_get_integer(wrap), 7);
-  candy_wrap_set_float(&wrap, 3.1415926f);
-  EXPECT_FLOAT_EQ(candy_wrap_get_float(wrap), 3.1415926f);
-  EXPECT_EQ(candy_wrap_get_integer(wrap), 3);
-  candy_wrap_delete(&wrap);
-  EXPECT_EQ((uint64_t)wrap, (uint64_t)NULL);
-}
-
-TEST(wrap, cast) {
-  struct candy_wrap *wrap = candy_wrap_none(0);
-  candy_wrap_set_string(&wrap, "hello world", sizeof("hello world"));
-  EXPECT_STREQ((char *)candy_wrap_get_string(wrap)->data, (char *)"hello world");
-  candy_wrap_set_integer(&wrap, 114514);
-  EXPECT_EQ(candy_wrap_get_integer(wrap), 114514);
-  candy_wrap_set_float(&wrap, 3.1415926f);
-  EXPECT_FLOAT_EQ(candy_wrap_get_float(wrap), 3.1415926f);
-  candy_wrap_delete(&wrap);
-  EXPECT_EQ((uint64_t)wrap, (uint64_t)NULL);
+TEST(wrap, lstring) {
+  candy_wrap_t wrap;
+  candy_wrap_init_string(&wrap, (char *)"hello world", sizeof("hello world"));
+  EXPECT_EQ(wrap.size, sizeof("hello world"));
+  EXPECT_STREQ(candy_wrap_get_string(&wrap, NULL), (char *)"hello world");
+  candy_wrap_deinit(&wrap);
 }
