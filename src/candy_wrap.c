501c43339c56f6738203c7131b2e65838bea14d1
ShunzDai <shunzdai@outlook.com>
2023-04-09 22:31:13 +0800

    perf(*):优化

--- src/candy_wrap.c
+++ src/candy_wrap.c
@@ -1,15 +1,16 @@
 #include "src/candy_wrap.h"
-#include "src/candy_buffer.h"
+#include "src/candy_lib.h"
 #include <string.h>
+#include <stdlib.h>
 
 void *candy_wrap_get_data(const candy_wrap_t *self) {
-  return candy_wrap_check_ldata(self, candy_wrap_sizeof(self)) ? (*(candy_buffer_t **)&self->data)->data : (void *)&self->data;
+  return candy_wrap_check_ldata(self, candy_wrap_sizeof(self)) ? *(void **)&self->data : (void *)&self->data;
 }
 
 void candy_wrap_set_data(candy_wrap_t *self, candy_wraps_t type, const void *data, size_t size, size_t n) {
   self->type = type;
   self->size = size;
-  memcpy(candy_wrap_check_ldata(self, n) ? ((*(candy_buffer_t **)&self->data) = candy_buffer_create(size, n, false))->data : (void *)&self->data, data, size * n);
+  memcpy(candy_wrap_check_ldata(self, n) ? (*(void **)&self->data = calloc(next_power2(size), n)) : (void *)&self->data, data, size * n);
 }
 
 void candy_wrap_init(candy_wrap_t *self) {
@@ -18,7 +19,7 @@ void candy_wrap_init(candy_wrap_t *self) {
 
 int candy_wrap_deinit(candy_wrap_t *self) {
   if (candy_wrap_check_ldata(self, candy_wrap_sizeof(self)))
-    candy_buffer_delete((candy_buffer_t **)&self->data);
+    free(*(void **)&self->data);
   memset(self, 0, sizeof(struct candy_wrap));
   return 0;
 }
