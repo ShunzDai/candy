81c756d54240b51bd45ec6612205c3b9cee1dab4
ShunzDai <shunzdai@outlook.com>
2023-05-18 12:44:46 +0800

    perf(vm):栈操作减少一层参数传递

--- src/candy_vm.h
+++ src/candy_vm.h
@@ -35,25 +35,14 @@ int candy_vm_deinit(candy_vm_t *self);
 
 int candy_vm_dump_global(candy_vm_t *self);
 
+void candy_vm_push(candy_vm_t *self, const candy_wrap_t *wrap);
+const candy_wrap_t *candy_vm_pop(candy_vm_t *self);
+
 int candy_vm_builtin(candy_vm_t *self, candy_regist_t list[], size_t size);
 int candy_vm_set_global(candy_vm_t *self, const char name[]);
 int candy_vm_get_global(candy_vm_t *self, const char name[]);
 int candy_vm_call(candy_vm_t *self, int nargs, int nresults);
 int candy_vm_execute(candy_vm_t *self, candy_block_t *block);
-
-void candy_vm_push_integer(candy_vm_t *self, const candy_integer_t val[], size_t size);
-void candy_vm_push_float(candy_vm_t *self, const candy_float_t val[], size_t size);
-void candy_vm_push_boolean(candy_vm_t *self, const candy_boolean_t val[], size_t size);
-void candy_vm_push_string(candy_vm_t *self, const char val[], size_t size);
-void candy_vm_push_ud(candy_vm_t *self, const void *val[], size_t size);
-void candy_vm_push_builtin(candy_vm_t *self, const candy_builtin_t val[], size_t size);
-
-const candy_integer_t *candy_vm_pull_integer(candy_vm_t *self, size_t *size);
-const candy_float_t *candy_vm_pull_float(candy_vm_t *self, size_t *size);
-const candy_boolean_t *candy_vm_pull_boolean(candy_vm_t *self, size_t *size);
-const char *candy_vm_pull_string(candy_vm_t *self, size_t *size);
-const void **candy_vm_pull_ud(candy_vm_t *self, size_t *size);
-const candy_builtin_t *candy_vm_pull_builtin(candy_vm_t *self, size_t *size);
 
 #ifdef __cplusplus
 }
