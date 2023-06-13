f6492ed824207c8d92d6aff3394195efdba4f7d2
ShunzDai <ShunzDai@outlook.com>
2023-06-13 18:56:59 +0800

    perf(vm):在try catch中执行`execute`

--- src/candy_table.c
+++ src/candy_table.c
@@ -67,17 +67,13 @@ static bool equal(const candy_wrap_t *keyl, const candy_wrap_t *keyr) {
 }
 
 int candy_table_fprint(const candy_wrap_t *self, FILE *out) {
-  static const char *type[] = {
-  #define CANDY_TYPE_STR
-  #include "src/candy_type.list"
-  };
   fprintf(out, "033[1;35m>>> table %p head\033[0m\n", self);
   fprintf(out, "pos  key-type         key-val  val-type         val-val\n");
   for (candy_pair_t *pair = _head(self); pair <= _tail(self); ++pair) {
-    fprintf(out, "%3ld", pair - _head(self));
-    fprintf(out, "%10s", type[pair->key.type]);
+    fprintf(out, "%3d", pair - _head(self));
+    fprintf(out, "%10s", candy_wrap_typestr(&pair->key));
     candy_wrap_fprint(&pair->key, out, 16);
-    fprintf(out, "%10s", type[pair->val.type]);
+    fprintf(out, "%10s", candy_wrap_typestr(&pair->val));
     candy_wrap_fprint(&pair->val, out, 16);
     fprintf(out, "\n");
   }
