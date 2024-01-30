7f603e94d0711a82226bbc94382c977f5c169c4e
ShunzDai <shunzdai@outlook.com>
2024-01-30 09:44:57 +0800

    perf(lexer):优化环形缓冲区扩张算法

--- src/candy_io.h
+++ src/candy_io.h
@@ -23,10 +23,6 @@ extern "C"{
 #include "src/candy_types.h"
 #include <setjmp.h>
 
-#if CANDY_DEFAULT_IO_SIZE < CANDY_LEXER_EXTRA_SIZE + CANDY_LEXER_LOOKAHEAD_SIZE
-#error "default io buffer size is too small"
-#endif /* CANDY_DEFAULT_IO_SIZE */
-
 #define candy_assert(_self, _condition, _type, _format, ...) ((_condition) ? ((void)0U) : candy_io_throw((_self), #_type " error: " _format, ##__VA_ARGS__))
 
 struct candy_io {
